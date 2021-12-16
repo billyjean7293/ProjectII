0dd220bbbc570c44917ea08b1d50aa69a139c86e
billyjean7293 <81057927+billyjean7293@users.noreply.github.com>
2021-12-16 16:39:22 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+/** @file scicall.h
+ *  @brief main.pp file
+           
+ *
+ *  main.pp file to execute program. 
+    Includes scical header file
+    
+ *
+ *  @author Billy Jean Baptiste
+ 
+ *  @bug No known bugs.
+ */
+
+
+
 #include "scical.h"
 
 #include <QApplication>
